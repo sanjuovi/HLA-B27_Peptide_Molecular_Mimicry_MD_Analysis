269a0b19f31e6a0bbd30423094dc40584f82a9ce
Sanju Singh <sci.sanju5@gmail.com>
2025-11-10 18:42:48 +0530

    Refactor post_md_analysis.sh to make it more clear
    Removed redundant RMSD, RMSF, Radius of Gyration, and SASA calculations for specific groups. Streamlined the script for efficiency.

--- scripts/scripts_md/post_md_analysis.sh
+++ scripts/scripts_md/post_md_analysis.sh
@@ -26,23 +26,11 @@ module load 2024r1 openmpi gromacs
 echo "\n🔹 RMSD Calculation: To check structural deviation from initial conformation"
 printf "19\n19\n" | gmx_mpi rms -s "$tpr" -f "$xtc" -o "${pep_id}_comp_rmsd.xvg" -n "$ndx"
 
-echo "\n🔹 RMSD Calculation: To check structural deviation from initial conformation"
-printf "17\n17\n" | gmx_mpi rms -s "$tpr" -f "$xtc" -o "${pep_id}_prot_rmsd.xvg" -n "$ndx"
-
-echo "\n🔹 RMSD Calculation: To check structural deviation from initial conformation"
-printf "18\n18\n" | gmx_mpi rms -s "$tpr" -f "$xtc" -o "${pep_id}_pep_rmsd.xvg" -n "$ndx"
-
 printf "4\n4\n" | gmx_mpi rms -s "$tpr" -f "$xtc" -o "${pep_id}_backbone_rmsd.xvg" -n "$ndx"
 
 # === RMSF ===
 echo "\n🔹 RMSF Calculation: To observe flexibility of each residue"
-gmx_mpi rmsf -s "$tpr" -f "$xtc" -o "${pep_id}_comp_rmsf.xvg" -res -n "$ndx" <<< "19"
-
-echo "\n🔹 RMSF Calculation: To observe flexibility of each residue"
 gmx_mpi rmsf -s "$tpr" -f "$xtc" -o "${pep_id}_pep_rmsf.xvg" -res -n "$ndx" <<< "18"
-
-echo "\n🔹 RMSF Calculation: To observe flexibility of each residue"
-gmx_mpi rmsf -s "$tpr" -f "$xtc" -o "${pep_id}_prot_rmsf.xvg" -res -n "$ndx" <<< "17"
 
 gmx_mpi rmsf -s "$tpr" -f "$xtc" -o "${pep_id}_backbone_rmsf.xvg" -res -n "$ndx" <<< "4"
 
@@ -51,19 +39,13 @@ echo "\n🔹 Radius of Gyration: To evaluate compactness"
 gmx_mpi gyrate -s "$tpr" -f "$xtc" -o "${pep_id}_prot_rgyr.xvg" -n "$ndx" <<< "17"
 
 echo "\n🔹 Radius of Gyration: To evaluate compactness"
-gmx_mpi gyrate -s "$tpr" -f "$xtc" -o "${pep_id}_pep_rgyr.xvg" -n "$ndx" <<< "18"
-
-echo "\n🔹 Radius of Gyration: To evaluate compactness"
 gmx_mpi gyrate -s "$tpr" -f "$xtc" -o "${pep_id}_comp_rgyr.xvg" -n "$ndx" <<< "19"
 
 gmx_mpi gyrate -s "$tpr" -f "$xtc" -o "${pep_id}_backbone_rgyr.xvg" -n "$ndx" <<< "4"
+
 # === SASA (Solvent Accessible Surface Area) ===
 echo "\n🔹 SASA Calculation: To study surface exposure"
-gmx_mpi sasa -s "$tpr" -f "$xtc" -o "${pep_id}_prot_sasa.xvg" -n "$ndx" <<< "17"
-
 gmx_mpi sasa -s "$tpr" -f "$xtc" -o "${pep_id}_pep_sasa.xvg" -n "$ndx" <<< "18"
-
-gmx_mpi sasa -s "$tpr" -f "$xtc" -o "${pep_id}_backbone_sasa.xvg" -n "$ndx" <<< "4"
 
 gmx_mpi sasa -s "$tpr" -f "$xtc" -o "${pep_id}_comp_sasa.xvg" -n "$ndx" <<< "19"
 # === Hydrogen Bonds ===
