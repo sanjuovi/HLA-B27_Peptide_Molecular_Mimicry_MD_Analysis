8dc241c18ec1ec4953c886218f9b19e1519d0f1e
Sanju Singh <sci.sanju5@gmail.com>
2025-11-26 10:45:56 +0530

    Refactor separate_binding_peptides function
    Updated input file name in example usage and added comments for clarity.

--- scripts/pep_mimicking/separate_binding_pep.py
+++ scripts/pep_mimicking/separate_binding_pep.py
@@ -1,16 +1,18 @@
+# Function definition to separate strong and weak binding peptides from NetMHCpan output file
 def separate_binding_peptides(input_file, strong_file='strong_binders.txt', weak_file='weak_binders.txt'):
-    with open(input_file, 'r') as infile:
-        lines = infile.readlines()
+    with open(input_file, 'r') as infile:           # it will open input result file and read all lines
+        lines = infile.readlines()                  # stores all lines in a list after reding them
 
+# Empty lists to store strong and weak binding peptide lines
     strong_lines = []
     weak_lines = []
 
-    for line in lines:
-        # Skip headers or empty lines
+    for line in lines:                      
+        # Loop through every line in the file and skip headers or empty lines
         if line.strip() == "" or line.startswith("#") or line.lower().startswith("pos") or line.lower().startswith("allele"):
             continue
 
-        # Check if SB or WB appears in the line
+        # Check if SB or WB appears in the line and store in respected file
         if 'SB' in line:
             strong_lines.append(line)
         elif 'WB' in line:
@@ -28,7 +30,7 @@ def separate_binding_peptides(input_file, strong_file='strong_binders.txt', weak
     print(f"📊 Total Strong: {len(strong_lines)}, Weak: {len(weak_lines)}")
 
 # Example usage:
-separate_binding_peptides("peptides_chunk1_netmpchen.txt")
+separate_binding_peptides("netmpchen_result_file.txt")      #change the name of .txt (input file) file accordingly
 
 
 
